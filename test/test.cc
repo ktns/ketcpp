dc7998e310e4943b9b08068af2029524e80eb2de
Katsuhiko Nishimra <ktns.87@gmail.com>
2015-05-13 20:44:18 +0900

    Implemented dumb test

---
+++ test/test.cc
@@ -0,0 +1,29 @@
+/*
+ * ketcpp: Quantum chemical toolset made of C++
+ * Copyright (C) 2015 Katsuhiko Nishimra
+ *
+ * This file is part of ketcpp.
+ *
+ * ketcpp is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * ketcpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ketcpp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "test.h"
+using namespace ketcpp;
+
+#include <igloo/igloo.h>
+using namespace igloo;
+
+Context(a_test) {
+  Spec(should_return_true) { Assert::That(value, Is().EqualTo(true)); }
+  bool value = test();
+};
