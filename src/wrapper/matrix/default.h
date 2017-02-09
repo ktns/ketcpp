07cfc25bd2201477de41d0c28ddc32f591dc7841
Katsuhiko Nishimra <ktns.87@gmail.com>
2017-02-10 01:00:28 +0900

    Define make_zero_matrix function

--- src/wrapper/matrix/default.h
+++ src/wrapper/matrix/default.h
@@ -22,14 +22,13 @@
 #include <memory>
 #include <utility>
 
-#include "wrapper/matrix/matrix.h"
 #include "wrapper/matrix/array.h"
 #include "wrapper/matrix/vector.h"
 
 namespace ketcpp {
   namespace wrapper {
     namespace matrix {
-      template <typename T, int m, int n = m>
+      template <typename T, size_t m, size_t n = m>
       Matrix<T>
       make_matrix(std::initializer_list<std::initializer_list<T>> list) {
         std::unique_ptr<MatrixBase<T>> ptr;
@@ -37,9 +36,8 @@ namespace ketcpp {
         return std::move(Matrix<T>(std::move(ptr)));
       }
 
-      template <typename T, int m, int n = m>
-      Matrix<T>
-      make_matrix(std::initializer_list<T> list) {
+      template <typename T, size_t m, size_t n = m>
+      Matrix<T> make_matrix(std::initializer_list<T> list) {
         std::unique_ptr<MatrixBase<T>> ptr;
         ptr.reset(new MatrixArray<T, m, n>(list));
         return std::move(Matrix<T>(std::move(ptr)));
@@ -51,6 +49,16 @@ namespace ketcpp {
         std::unique_ptr<MatrixBase<T>> ptr;
         ptr.reset(new MatrixVector<T>(list));
         return std::move(Matrix<T>(std::move(ptr)));
+      }
+
+      template <typename T> Matrix<T> make_zero_matrix(size_t m, size_t n = 0) {
+        if (n == 0)
+          n = m;
+        assert(n > 0 && m > 0);
+        auto ptr = std::make_unique<MatrixVector<T>>(n, m);
+        ptr->for_each(
+            [&ptr](size_t i, size_t j) { assert(ptr->at(i, j) == 0); });
+        return Matrix<T>(std::move(ptr));
       }
     }
   }
---
+++ test/wrapper/matrix/default.cc
@@ -0,0 +1,35 @@
+/*
+ * ketcpp: Quantum chemical toolset made of C++
+ * Copyright (C) 2015 Katsuhiko Nishimra
+ *
+ * This file is part of ketcpp.
+ *
+ * ketcpp is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * ketcpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ketcpp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <bandit/bandit.h>
+
+#include "wrapper/matrix/default.h"
+using namespace bandit;
+using namespace bandit::Matchers;
+using namespace ketcpp::wrapper::matrix;
+
+go_bandit([] {
+  describe("make_zero_matrix", [] {
+    it("should return a matrix whose elements are all zero", [] {
+      auto mat = make_zero_matrix<double>(2, 3);
+      mat->for_each(
+          [&mat](size_t i, size_t j) { mat->at(i, j) must equal(0); });
+    });
+  });
+});
