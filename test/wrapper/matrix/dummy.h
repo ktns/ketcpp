8630f8ca1db2a9ba2e890d91a9f81059e7d24037
Katsuhiko Nishimra <ktns.87@gmail.com>
2017-02-12 17:57:58 +0900

    Implement default constructable dummy matrix for tests

---
+++ test/wrapper/matrix/dummy.h
@@ -0,0 +1,60 @@
+/*
+ * ketcpp: Quantum chemical toolset made of C++
+ * Copyright (C) 2017 Katsuhiko Nishimra
+ *
+ * This file is part of ketcpp.
+ *
+ * ketcpp is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * ketcpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ketcpp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "wrapper/matrix/matrix.h"
+
+namespace ketcpp::wrapper::matrix {
+  template <typename T> class MatrixDummy : public MatrixBase<T> {
+  public:
+    MatrixDummy() {}
+
+    size_t get_num_rows() const {
+      throw std::logic_error("Dummy matrix is in use!");
+    }
+    size_t get_num_columns() const {
+      throw std::logic_error("Dummy matrix is in use!");
+    };
+    size_t get_row_size() const {
+      throw std::logic_error("Dummy matrix is in use!");
+    };
+    size_t get_column_size() const {
+      throw std::logic_error("Dummy matrix is in use!");
+    };
+    T &at(size_t, size_t) {
+      throw std::logic_error("Dummy matrix is in use!");
+    };
+    T at(size_t, size_t) const {
+      throw std::logic_error("Dummy matrix is in use!");
+    };
+    std::unique_ptr<MatrixBase<T>> copy() const {
+      return std::make_unique<MatrixDummy>();
+    };
+  };
+
+  template <typename T> Matrix<T> make_dummy_matrix() {
+    return Matrix<T>(std::make_unique<MatrixDummy<T>>());
+  }
+
+  template <typename T>
+  std::ostream &operator<<(std::ostream &os, const MatrixDummy<T> &dummy) {
+    return os << "{Dummy Matrix}";
+  }
+}
