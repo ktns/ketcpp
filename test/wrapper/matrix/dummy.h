ef09c2e56673319d390c453df86c92764c9d7683
Katsuhiko Nishimra <ktns.87@gmail.com>
2017-02-28 21:45:13 +0900

    Update license notions

--- test/wrapper/matrix/dummy.h
+++ test/wrapper/matrix/dummy.h
@@ -6,7 +6,7 @@
  *
  * ketcpp is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or any later version.
+ * Foundation, either version 3.0 of the License, or any later version.
  *
  * ketcpp is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
