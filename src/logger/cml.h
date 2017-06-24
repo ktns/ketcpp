0800ebe0fc0cf75b5d00d1a41dfa51e7cda87b6e
Katsuhiko Nishimra <ktns.87@gmail.com>
2017-06-24 13:48:28 +0900

    CMLLogger skeleton

---
+++ src/logger/cml.h
@@ -0,0 +1,31 @@
+/*
+ * ketcpp: Quantum chemical toolset made of C++
+ * Copyright (C) 2017 Katsuhiko Nishimra
+ *
+ * This file is part of ketcpp.
+ *
+ * ketcpp is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * ketcpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ketcpp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <ostream>
+
+namespace ketcpp::logger {
+  //! Output a log of a job in CML CompChem convention
+  class CMLLogger {
+  public:
+    CMLLogger() {}
+    ~CMLLogger() {}
+  };
+}
